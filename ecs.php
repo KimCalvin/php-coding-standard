a0cf371a6e50a101e6f6825ddcaf1e4794085f69
Kim Schiefke <k.schiefke@demv.de>
2023-05-12 14:39:16 +0200

    update dependencies, migrate ecs.php to new config format

--- ecs.php
+++ ecs.php
@@ -7,56 +7,44 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $services->set(ArraySyntaxFixer::class)
-        ->call('configure', [[
-            'syntax' => 'short',
-        ]]);
-
-    $services->set(PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer::class)
-        ->call('configure', [[
-            'const' => 'single',
-            'property' => 'single',
-            'method' => 'multi',
-        ]]);
-
-    $services->set(PhpCsFixer\Fixer\Import\OrderedImportsFixer::class)
-        ->call('configure', [[
-            'sort_algorithm' => 'alpha',
-            'imports_order' => ['class', 'function', 'const'],
-        ]]);
-
-    $services->set(PhpCsFixer\Fixer\Import\GlobalNamespaceImportFixer::class)
-        ->call('configure', [[
-            'import_classes' => true,
-            'import_constants' => true,
-            'import_functions' => true,
-        ]]);
-
-    $services->set(PhpCsFixer\Fixer\PhpUnit\PhpUnitMethodCasingFixer::class)
-        ->call('configure', [[
-            'case' => 'snake_case',
-        ]]);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::LINE_ENDING, "\n");
-    $parameters->set(Option::PATHS, [
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
         __DIR__ . '/bin',
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
-
-    $parameters->set(Option::SETS, [
+    $ecsConfig->lineEnding('\n');
+    $ecsConfig->sets([
         SetList::COMMON,
         SetList::CLEAN_CODE,
-        // SetList::DEAD_CODE,
         SetList::PSR_12,
-        // SetList::PHP_70,
-        // SetList::PHP_71,
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
+            'syntax' => 'short',
+        ]);
+
+    $ecsConfig->ruleWithConfiguration(PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer::class, [
+        'const' => 'single',
+        'property' => 'single',
+        'method' => 'multi',
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(PhpCsFixer\Fixer\Import\OrderedImportsFixer::class, [
+        'sort_algorithm' => 'alpha',
+        'imports_order' => ['class', 'function', 'const'],
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(PhpCsFixer\Fixer\Import\GlobalNamespaceImportFixer::class, [
+        'import_classes' => true,
+        'import_constants' => true,
+        'import_functions' => true,
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(PhpCsFixer\Fixer\PhpUnit\PhpUnitMethodCasingFixer::class, [
+        'case' => 'snake_case',
     ]);
 };
